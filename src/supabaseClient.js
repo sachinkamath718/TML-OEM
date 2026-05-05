1a6c560777b2bd369e5eeac3313251b701b94840
zeliotmarketing <marketing@zeliot.in>
2026-05-05 14:16:02 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://your-project-id.supabase.co';   // 👈 replace this
-const SUPABASE_ANON_KEY = 'your-anon-key-here';               // 👈 replace this
+const SUPABASE_URL = 'https://dfewivwmtnmwuikkjdor.supabase.co';   // 👈 replace this
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmZXdpdndtdG5td3Vpa2tqZG9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NjEzOTksImV4cCI6MjA5MzUzNzM5OX0.hWyDq2KhqSEwRZlH52e4d0UmbjtYPe5X0JQoGhvSs4E';               // 👈 replace this
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
